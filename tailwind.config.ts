3bbda8c565a71f606a3585bcedc3860f8aa0ecba
John Chinonso Edeh <netojaycee@gmail.com>
2025-07-08 10:57:54 +0100

    G

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,10 +14,9 @@ const config = {
   		center: true,
   		padding: '2rem',
   		screens: {
-  			'2xl': '1400px',
-			'xs': '475px',
+  			'2xl': '1400px'
 
-  		}
+  		},
   	},
   	extend: {
   		keyframes: {
